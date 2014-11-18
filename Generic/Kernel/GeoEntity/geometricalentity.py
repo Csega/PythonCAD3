07658e6dbdea5d12b5e847b2ff0161664fb52cc9
Gabor Cseh <csega@mailbox.hu>
2014-11-18 12:22:51 +0100

    exception handling changed to Python 3 & PEP 8 changes

--- Generic/Kernel/GeoEntity/geometricalentity.py
+++ Generic/Kernel/GeoEntity/geometricalentity.py
@@ -22,8 +22,8 @@
 #
 import math
 
-import sympy            as mainSympy
-import sympy.geometry   as geoSympy
+import sympy as mainSympy
+import sympy.geometry as geoSympy
 
 class GeometricalEntity(dict):
     """
@@ -35,23 +35,23 @@ class GeometricalEntity(dict):
         """
         if kw is None and argNameType is None:
             return
-        if len(kw)!=len(argNameType):
-            raise TypeError, "Wrong number of items "
+        if len(kw) != len(argNameType):
+            raise TypeError("Wrong number of items ")
         for k in kw:
             if k in argNameType:
                 if isinstance(kw[k],argNameType[k]):
                     self[k]=kw[k]
                 else:
-                    raise TypeError, "Wrong Type for argument %s"%str(k)
+                    raise TypeError("Wrong Type for argument %s"%str(k))
             else:
-                raise TypeError, "Wrong argument %s "%str(k)
-        self.arguments=argNameType
-        self._snapPoints=[]
+                raise TypeError("Wrong argument %s "%str(k))
+        self.arguments = argNameType
+        self._snapPoints = []
     
-    def updateSnapPoint(self, force=None, fromPoint=None, fromEnt=None):
+    def updateSnapPoint(self, force = None, fromPoint = None, fromEnt = None):
         pass
     
-    def getUpdatedSnapPoints(self, force, fromPoint=None,  fromEnt=None):
+    def getUpdatedSnapPoints(self, force, fromPoint = None,  fromEnt = None):
         """
             update the snappoint with force argument and return an array of geopoint
         """
@@ -67,7 +67,7 @@ class GeometricalEntity(dict):
         
     @snapPoints.setter
     def snapPoints(self, value):
-        self._snapPoints=value
+        self._snapPoints = value
         
     def getArgumentsName(self):
         """
@@ -85,7 +85,7 @@ class GeometricalEntity(dict):
         """
             set the construction elemtnts
         """
-        self=kw
+        self = kw
         
     def move(self, fromPoint, toPoint):
         """
@@ -93,10 +93,10 @@ class GeometricalEntity(dict):
         """
         from Kernel.GeoUtil.geolib import Vector
         from Kernel.GeoEntity.point import Point
-        v=Vector(fromPoint, toPoint)
+        v = Vector(fromPoint, toPoint)
         for key in self:
-            if isinstance(self[key] , Point):
-                self[key]+=v.point
+            if isinstance(self[key], Point):
+                self[key] += v.point
         return v.point
     
     def rotate(self, rotationPoint, angle):
@@ -107,10 +107,10 @@ class GeometricalEntity(dict):
         from Kernel.GeoEntity.point import Point
         
         for key in self:
-            if isinstance(self[key] , Point):
-                v=Vector(rotationPoint,self[key] )
+            if isinstance(self[key], Point):
+                v = Vector(rotationPoint, self[key])
                 v.rotate(angle)
-                self[key]=rotationPoint+v.point
+                self[key] = rotationPoint + v.point
         
     
     def getSympy(self):
@@ -133,25 +133,25 @@ class GeometricalEntityComposed(dict):
     def __init__(self, kw, argNameType):
         if kw is None and argNameType is none:
             return
-        if len(kw)!=len(argNameType):
-            raise TypeError, "Wrong number of items "
+        if len(kw) != len(argNameType):
+            raise TypeError("Wrong number of items ")
         for k in kw:
             if k in argNameType:
-                newTest=argNameType[k]
+                newTest = argNameType[k]
                 if isinstance(argNameType[k], tuple):
                     if None in argNameType[k]:
                         if kw[k] == None:
-                            self[k]=kw[k]
+                            self[k] = kw[k]
                             continue
                         else:
-                            newTest=tuple([x for x in argNameType[k] if x!=None])
+                            newTest = tuple([x for x in argNameType[k] if x != None])
                 if isinstance(kw[k],newTest):
-                    self[k]=kw[k]
+                    self[k] = kw[k]
                 else:
-                    raise TypeError, "Wrong Type for argument %s"%str(k)
+                    raise TypeError("Wrong Type for argument %s"%str(k))
             else:
-                raise TypeError, "Wrong argument %s "%str(k)
-        self.arguments=argNameType
+                raise TypeError("Wrong argument %s "%str(k))
+        self.arguments = argNameType
     
     def getArgumentsName(self):
         """
