754f8745ad62cd2eae78c12f71e79a5a2bff0439
Gabor Cseh <csega@mailbox.hu>
2014-11-19 15:18:18 +0100

    exceptions -> Python 3, has_key() removed, PEP 8

--- Generic/Kernel/GeoEntity/style.py
+++ Generic/Kernel/GeoEntity/style.py
@@ -21,40 +21,42 @@
 # This class provide all the style operation for the pythoncadDB
 #
 
-from Kernel.GeoEntity.geometricalentity       import GeometricalEntity
-from Kernel.GeoUtil.util                      import getRandomString
+from Kernel.GeoEntity.geometricalentity import GeometricalEntity
+from Kernel.GeoUtil.util import getRandomString
 
 class Style(GeometricalEntity):
         """
             This class rappresent the style in pythoncad
             objID is the object that rappresent the id in the db
         """
-        def __init__(self,kw):
+        def __init__(self, kw):
             """
                 Initialize a Arc/Circle.
                 kw['STYLE_0'] 
                 kw['STYLE_1'] 
             """
-            argDescription=dict([(key,str) for key in kw])
-            GeometricalEntity.__init__(self,kw, argDescription)
+            argDescription = dict([(key, str) for key in kw])
+            GeometricalEntity.__init__(self, kw, argDescription)
             
             if self.has_key('STYLE_1'):
-                if self['STYLE_1'] ==None:
+                if self['STYLE_1'] == None:
                     from Kernel.initsetting import getDefaultStyle
-                    self.__styleProperty=getDefaultStyle()
+                    self.__styleProperty = getDefaultStyle()
             else:
                 from Kernel.initsetting import getDefaultStyle
-                self['STYLE_1']=getDefaultStyle()
-            if self.has_key('STYLE_0'):
-                if self['STYLE_0'] ==None:
-                    self.name=getRandomString()       
+                self['STYLE_1'] = getDefaultStyle()
+            #if self.has_key('STYLE_0'):  # old Python 2 syntax
+            if 'STYLE_0' in self:
+                if self['STYLE_0'] == None:
+                    self.name = getRandomString()       
             else:
-                self['STYLE_0']=getRandomString()       
+                self['STYLE_0'] = getRandomString()       
+        
         def setName(self, name):
             """
                 set the name of the style
             """
-            self['STYLE_0']=name
+            self['STYLE_0'] = name
 
         def getName(self):
             """
@@ -62,14 +64,14 @@ class Style(GeometricalEntity):
             """
             return self['STYLE_0']
         
-        name=property(setName, getName, None, "Style Name")
+        name = property(setName, getName, None, "Style Name")
         
         def getStyleProp(self, name):
             """
                 get the style property
             """
-            if name in  self['STYLE_1']:
-                return  self['STYLE_1'][name]
+            if name in self['STYLE_1']:
+                return self['STYLE_1'][name]
             else:
                 return None
         
@@ -77,9 +79,9 @@ class Style(GeometricalEntity):
             """
                 set the style property 
             """
-            from Kernel.initsetting         import PYTHONCAD_STYLE_ATTRIBUTES
-            from Kernel.exception           import EntityMissing
+            from Kernel.initsetting import PYTHONCAD_STYLE_ATTRIBUTES
+            from Kernel.exception import EntityMissing
             if name in PYTHONCAD_STYLE_ATTRIBUTES:
-                self['STYLE_1'][name]=value
+                self['STYLE_1'][name] = value
             else:
-                raise EntityMissing,"Unable to find the property %s"%str(name)
+                raise EntityMissing("Unable to find the property %s" % str(name))
