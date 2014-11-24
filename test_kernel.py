3d621b547ade8e8cbf5edac38a3a0e5fe852a4a6
Gabor Cseh <csega@mailbox.hu>
2014-11-24 15:05:21 +0100

    converting to Python 3 and PEP 8 - started

--- test_kernel.py
+++ test_kernel.py
@@ -10,88 +10,88 @@ import sqlite3 as sqlite
 # This is needed for me to use unpickle objects
 sys.path.append(os.path.join(os.getcwd(), 'Generic', 'Kernel'))    
     
-from random                             import random
-from Generic.Kernel.document            import *
-from Generic.Kernel.application         import Application
-from Generic.Kernel.Entity.point        import Point
-from Generic.Kernel.Entity.style        import Style
-from Generic.Kernel.Entity.segjoint     import Chamfer
+from random import random
+from Generic.Kernel.document import *
+from Generic.Kernel.application import Application
+from Generic.Kernel.Entity.point import Point
+from Generic.Kernel.Entity.style import Style
+from Generic.Kernel.Entity.segjoint import Chamfer
 
-def printId(kernel,obj):
+def printId(kernel, obj):
     """
         print the id of the obj
     """
-    print "Save Entity: %s"%str(type(obj))
+    print("Save Entity: %s" % str(type(obj)))
     
 def testSinglePoint(kernel):
     """
         test single point operation
     """
-    startTime=time.clock()
-    print "Create a single point"
-    basePoint=Point(10,1)
-    print "singlePoint ",type(basePoint)
+    startTime = time.clock()
+    print("Create a single point")
+    basePoint = Point(10, 1)
+    print("singlePoint ", type(basePoint))
     kernel.saveEntity(basePoint)
-    endTime=time.clock()-startTime
-    print "Time for saving  a single point   %ss"%str(endTime)   
+    endTime = time.clock() - startTime
+    print("Time for saving  a single point   %ss" % str(endTime))
     
-def testMultiPoints(kernel,nPoint)    :
+def testMultiPoints(kernel, nPoint):
     """
         test the point operatoin
     """
-    startTime=time.clock()
+    startTime = time.clock()
     kernel.startMassiveCreation()
     for i in range(nPoint):
-        basePoint=Point(10,i)
+        basePoint = Point(10, i)
         kernel.saveEntity(basePoint)
     kernel.performCommit()
-    endTime=time.clock()-startTime
-    print "Create n: %s entity in : %ss"%(str(nPoint ),str(endTime))
+    endTime = time.clock() - startTime
+    print("Create n: %s entity in : %ss" % (str(nPoint ), str(endTime)))
 
-def testMultiSegments(kernel,nSegments):
+def testMultiSegments(kernel, nSegments):
     """
         create a single segment
     """    
-    startTime=time.clock()
+    startTime = time.clock()
     kernel.startMassiveCreation()
     for i in range(nSegments):
-        _p1=Point(10,i)
-        _p2=Point(10,i)
-        _s=Segment(_p1,_p2)
+        _p1 = Point(10, i)
+        _p2 = Point(10, i)
+        _s = Segment(_p1, _p2)
         kernel.saveEntity(_s)
     kernel.performCommit()
-    endTime=time.clock()-startTime
-    print "Create n: %s entity in : %ss"%(str(nSegments ),str(endTime))
+    endTime = time.clock() - startTime
+    print("Create n: %s entity in : %ss" % (str(nSegments ), str(endTime)))
     
 def testSingleSegment(kernel):
     """
         create a single segment
     """    
-    _p1=Point(10,10)
-    _p2=Point(10,20)
-    _s=Segment(_p1,_p2)
+    _p1 = Point(10, 10)
+    _p2 = Point(10, 20)
+    _s = Segment(_p1, _p2)
     kernel.saveEntity(_s)
 
 def testGetLayerEnt(kernel):
     """
         get layer dictionary of all the id child
     """
-    startTime=time.clock()
-    ids=kernel.getLayerChild('ROOT')
-    nids=len(ids)
-    endTime=time.clock()-startTime
-    print "Get n: %s layer entity in : %ss"%(str(nids ),str(endTime))
+    startTime = time.clock()
+    ids = kernel.getLayerChild('ROOT')
+    nids = len(ids)
+    endTime = time.clock()-startTime
+    print("Get n: %s layer entity in : %ss" % (str(nids ), str(endTime)))
 
 def testPerformanceInCreation(kr):
-    print "Points:"
-    testMultiPoints(kr,1)
+    print("Points:")
+    testMultiPoints(kr, 1)
     #testMultiPoints(kr,10)
     #testMultiPoints(kr,100)
     #testMultiPoints(kr,1000)
     #testMultiPoints(kr,10000)
     #testMultiPoints(kr,100000)
-    print "Segments:"
-    testMultiSegments(kr,1)
+    print("Segments:")
+    testMultiSegments(kr, 1)
     #testMultiSegments(kr,10)
     #testMultiSegments(kr,100)
     #testMultiSegments(kr,1000)
@@ -103,17 +103,17 @@ def createSegment(kernel):
     """
         create a single segment
     """    
-    _p1=Point(10,10)
-    _p2=Point(10,20)
-    _s=Segment(_p1,_p2)
+    _p1 = Point(10, 10)
+    _p2 = Point(10, 20)
+    _s = Segment(_p1, _p2)
     return kernel.saveEntity(_s)
 
 def CreateModifieEntity(kr):
     """
         test for create and modifie an entity
     """
-    ent=createSegment(kr)
-    celement={'POINT_1':Point(100,100), 'POINT_2':Point(200,200)}
+    ent = createSegment(kr)
+    celement = {'POINT_1': Point(100, 100), 'POINT_2': Point(200, 200)}
     ent.setConstructionElement(celement)
     kr.saveEntity(ent)
  
@@ -125,9 +125,9 @@ def deleteTable(tableName):
     # sqlite + R*Tree module
     from pysqlite2 import dbapi2 as sql
 
-    dbPath='pythoncad.pdr' 
+    dbPath = 'pythoncad.pdr'
     dbConnection = sql.connect(dbPath)
-    statment="drop table pycadrel"
+    statment ="drop table pycadrel"
     _cursor = dbConnection.cursor()
     _rows = _cursor.execute(statment)
     dbConnection.commit()
@@ -137,14 +137,14 @@ def testPointDb(nLoop):
     """
     Test point creation 
     """
-    #import sqlite3 as sql
+    # import sqlite3 as sql
     # sqlite + R*Tree module
     from pysqlite2 import dbapi2 as sql
     import cPickle as pickle
     
     dbConnection = sql.connect(":memory:")
-    cursor=dbConnection.cursor()
-    _sqlCreation="""CREATE TABLE pycadent(
+    cursor = dbConnection.cursor()
+    _sqlCreation = """CREATE TABLE pycadent(
                     pycad_id INTEGER PRIMARY KEY,
                     pycad_entity_id INTEGER,
                     pycad_object_type TEXT,
@@ -162,49 +162,49 @@ def testPointDb(nLoop):
                     pycad_bbox_xmax REAL,
                     pycad_bbox_ymax REAL)"""
     cursor.execute(_sqlCreation)
-    dic=[(x, y) for x in range(20) for y in range(1)]
+    dic = [(x, y) for x in range(20) for y in range(1)]
     dbConnection.commit()
-    startTime=time.clock()
+    startTime = time.clock()
     for i in range(nLoop):
-        dumpDic=pickle.dumps(dic)
-        sql="""insert into pycadent (pycad_entity_id,pycad_object_definition) values(1,"%s")"""%str(dumpDic)
+        dumpDic = pickle.dumps(dic)
+        sql = """insert into pycadent (pycad_entity_id,pycad_object_definition) values(1,"%s")""" % str(dumpDic)
         cursor.execute(sql)
     dbConnection.commit()
-    endTime=time.clock()-startTime
-    everage=endTime/nLoop
-    print "End time for nLoop %s in %s everage %s "%(str(nLoop), str(endTime), str(everage))
+    endTime = time.clock() - startTime
+    everage = endTime / nLoop
+    print("End time for nLoop %s in %s everage %s " % (str(nLoop), str(endTime), str(everage)))
 
 def testPointDb1(nLoop):
     """
     Test point creation 
     """
-    #import sqlite3 as sql
+    # import sqlite3 as sql
     # sqlite + R*Tree module
     from pysqlite2 import dbapi2 as sql
-    import cPickle as pickle
+    import pickle
     
     dbConnection = sql.connect(":memory:")
-    cursor=dbConnection.cursor()
-    _sqlCreation="""CREATE TABLE pycadent(
+    cursor = dbConnection.cursor()
+    _sqlCreation = """CREATE TABLE pycadent(
                     pycad_X REAL,
                     pycad_Y REAL)"""
     cursor.execute(_sqlCreation)
     dbConnection.commit()    
-    dic=[(x,y) for x in range(20) for y in range(1)]
-    startTime=time.clock()
+    dic = [(x,y) for x in range(20) for y in range(1)]
+    startTime = time.clock()
     for i in range(nLoop):
         for x,y in dic:
-            _sql="""INSERT INTO pycadent (pycad_X,pycad_Y) 
-                VALUES (%s,%s)"""%(str(x),str(y))
+            _sql = """INSERT INTO pycadent (pycad_X,pycad_Y) 
+                VALUES (%s,%s)""" % (str(x),str(y))
             cursor.execute(_sql)
     dbConnection.commit()   
-    endTime=time.clock()-startTime
-    everage=endTime/nLoop
-    print "End time for nLoop %s in %s everage %s "%(str(nLoop), str(endTime), str(everage))
+    endTime = time.clock() - startTime
+    everage = endTime / nLoop
+    print("End time for nLoop %s in %s everage %s " % (str(nLoop), str(endTime), str(everage)))
 
 def TestcPickleSql():
     for i in [10,100, 1000, 1000, 10000, 100000]:
-        print ">>print Test with %s entitys"%str(i)
+        print(">>print Test with %s entitys" % str(i))
         testPointDb(i)
         testPointDb1(i)
 
@@ -212,14 +212,14 @@ def getAllSegment(kr):
     """
         get all the segments
     """
-    print ">>Looking for segment"
-    ent=kr.getEntityFromType('SEGMENT')
+    print(">>Looking for segment")
+    ent = kr.getEntityFromType('SEGMENT')
     for e in ent:
-        print "e >>>>", e.eType
+        print("e >>>>", e.eType)
        
 def test():
-    print ">>Create pycad object"
-    kr=PyCadDbKernel()
+    print(">>Create pycad object")
+    kr = PyCadDbKernel()
     #CreateModifieEntity(kr)
     #print "Add creation event"
     #kr.saveEntityEvent+=printId
@@ -233,143 +233,147 @@ class ioKernel(object):
         PythonCad Kernel
     """
     def __init__(self):
-        startTime=time.clock()
-        self.__kr=PyCadDbKernel()
-        endTime=time.clock()-startTime    
-        print "Inizialize kernel object in : %s"%str(endTime)
-        self.__command={}
-        #Basic Command
-        self.__command['H']=self.help
-        self.__command['Esc']=self.endApplication
-        self.__command['Import']=self.importExt
-        self.__command['SetCurrentLayer']=self.setCurrentLayer
-        #Creatio Command
-        self.__command['NewSegment']=self.newSegment
-        self.__command['NewArc']=self.newArc
-        self.__command['AddLayer']=self.addLayer
-        #Delete Command
-        self.__command['Delete']=self.delete
-        self.__command['DeleteLayer']=self.deleteLayer
-        #Get Command
-        self.__command['GetCurrentLayer']=self.getCurrentLayerName
-        self.__command['GetEntType']=self.getEntType
-        self.__command['GetLayers']=self.getLayers
-        self.__command['GetDrwEnt']=self.getDrawingEntity
-        #Edit Command
-        self.__command['UnDo']=self.unDo
-        self.__command['ReDo']=self.reDo
-        #WorkFlow Command
-        self.__command['Relese']=self.release
-        #View Command
-        self.__command['Hide']=self.hideEntity
-        self.__command['UnHide']=self.unHideEntity
-        self.__kr.handledError=self.printError
+        startTime = time.clock()
+        self.__kr = PyCadDbKernel()
+        endTime = time.clock() - startTime    
+        print("Inizialize kernel object in : %s" % str(endTime))
+        self.__command = {}
+        # Basic Command
+        self.__command['H'] = self.help
+        self.__command['Esc'] = self.endApplication
+        self.__command['Import'] = self.importExt
+        self.__command['SetCurrentLayer'] = self.setCurrentLayer
+        # Creatio Command
+        self.__command['NewSegment'] = self.newSegment
+        self.__command['NewArc'] = self.newArc
+        self.__command['AddLayer'] = self.addLayer
+        # Delete Command
+        self.__command['Delete'] = self.delete
+        self.__command['DeleteLayer'] = self.deleteLayer
+        # Get Command
+        self.__command['GetCurrentLayer'] = self.getCurrentLayerName
+        self.__command['GetEntType'] = self.getEntType
+        self.__command['GetLayers'] = self.getLayers
+        self.__command['GetDrwEnt'] = self.getDrawingEntity
+        # Edit Command
+        self.__command['UnDo'] = self.unDo
+        self.__command['ReDo'] = self.reDo
+        # WorkFlow Command
+        self.__command['Relese'] = self.release
+        # View Command
+        self.__command['Hide'] = self.hideEntity
+        self.__command['UnHide'] = self.unHideEntity
+        self.__kr.handledError = self.printError
         
     def printError(self, **args):
         """
             print the error/warning caming from the kernel
         """    
-        if args.has_key('error'):
-            if args['error']=='DxfReport':
-                print "dxfReport" #todo : improve this sistem
-            elif args['error']=='DxfUnsupportedFormat':
-                print "DxfUnsupportedFormat" #todo : improve this sistem
+        # if args.has_key('error'):
+        if 'error' in args:
+            if args['error'] == 'DxfReport':
+                print("dxfReport")  # todo : improve this system
+            elif args['error'] == 'DxfUnsupportedFormat':
+                print("DxfUnsupportedFormat")  # todo : improve this system
                 
     def mainLoop(self):
         """
             mainLoop operation
         """
         while 1:
-            imputstr=raw_input("Insert a command (H for Help)>> :")
+            imputstr = input("Insert a command (H for Help)>> :")
             if self.__command.has_key(imputstr):
                 self.__command[imputstr]()
             else:
-                print "Wrong Command !!"
+                print("Wrong Command !!")
+    
     def help(self):
         """
             print the help
         """
-        print "*"*10
-        print "PyCadIOInterface Help"
-        kmd=self.__command.keys()
+        print("*" * 10)
+        print("PyCadIOInterface Help")
+        kmd = self.__command.keys()
         kmd.sort()
         for s in kmd:
-            print "command :" , s
-        print "*"*10   
+            print("command :" , s)
+        print("*"*10)
         
     def newSegment(self):
         """
             create a new segment
         """
         try:
-            val=(raw_input("-->Get First Point x,y :"))
-            p1=Point(val[0], val[1])
-            val=(raw_input("-->Get Second Point x,y :"))
-            p2=Point(val[0], val[1])
-            _s=Segment(p1,p2)
+            val = (input("-->Get First Point x,y :"))
+            p1 = Point(val[0], val[1])
+            val = (input("-->Get Second Point x,y :"))
+            p2 = Point(val[0], val[1])
+            _s = Segment(p1,p2)
             self.__kr.saveEntity(_s)
         except:
-            print "---->Error on point creation !!"
+            print("---->Error on point creation !!")
             
     def getDrawingEntity(self):        
         """
             get all the drawing entity
         """
-        print "--<< Looking for All entitys"
-        startTime=time.clock()
-        ents=self.__kr.getAllDrawingEntity()
-        endTime=time.clock()-startTime       
+        print("--<< Looking for All entitys")
+        startTime = time.clock()
+        ents = self.__kr.getAllDrawingEntity()
+        endTime = time.clock() - startTime       
         printEntity(ents)
-        print "Exec query get %s ent in %s s"%(str(len(ents)), str(endTime))
-        print "********************************"
+        print("Exec query get %s ent in %s s" % (str(len(ents)), str(endTime)))
+        print("********************************")
 
     def reDo(self):
         """
             perform the redo command
         """
         try:
-            print "-->>Perform Redo"
+            print("-->>Perform Redo")
             self.__kr.reDo()  
         except UndoDb:
-            print "----<<Err>>No more redo to performe"
+            print("----<<Err>>No more redo to perform")
+
     def unDo(self):
         """
             perform the undo command
         """
         try:
-            print "-->>Perform unDo"
+            print("-->>Perform unDo")
             self.__kr.unDo()  
         except UndoDb:
-            print "----<<Err>>No more unDo to performe"
+            print("----<<Err>>No more unDo to perform")
+    
     def delete(self):
         """
             Delete the entity
         """
         try:
-            val=(raw_input("-->Write entityId :"))
+            val = (input("-->Write entityId :"))
             self.__kr.deleteEntity(val)
         except:
-            print "----<<Err>>Enable to delete the entity"
+            print("----<<Err>>Enable to delete the entity")
 
     def hideEntity(self):
         """
             hide an entity
         """
-        entId=raw_input("-->Insert the id to hide :")
+        entId = raw_input("-->Insert the id to hide :")
         try:
             self.__kr.hideEntity(entityId=entId)
         except:
-            print "----<<Err>>On Hide the id : %s "%entId
+            print("----<<Err>>On Hide the id : %s " % entId)
 
     def unHideEntity(self):
         """
             unhide an entity
         """
-        entId=raw_input("-->Insert the id to Unhide :")
+        entId = raw_input("-->Insert the id to Unhide :")
         try:
             self.__kr.unHideEntity(entityId=entId)
         except:
-            print "----<<Err>>On unHide the id : %s "%entId
+            print("----<<Err>>On unHide the id : %s " % entId)
         
     def endApplication(self):
         """
@@ -382,51 +386,53 @@ class ioKernel(object):
             release the current drawing
         """
         self.__kr.release()
+    
     def getLayers(self):
         """
             get all layer
         """
         try:
-            print ">>** Layer Tree ****"
-            layersTree=self.__kr.getLayerTree().getLayerTree()
+            print(">>** Layer Tree ****")
+            layersTree = self.__kr.getLayerTree().getLayerTree()
             printTree(layersTree, 1)
-            print "<<** Layer Tree ****"
+            print("<<** Layer Tree ****")
         except:
-            print "----<<Err>>On getLayers "
+            print("----<<Err>>On getLayers ")
+    
     def addLayer(self):
         """
             add a new layer
         """
-        layerName=raw_input("-->Insert LayerName :")
+        layerName = input("-->Insert LayerName :")
         try:
-            activeLayer=self.__kr.getLayerTree().getActiveLater()
-            newLayer=Layer(layerName)
-            dbLayer=self.__kr.saveEntity(newLayer)
+            activeLayer = self.__kr.getLayerTree().getActiveLater()
+            newLayer = Layer(layerName)
+            dbLayer = self.__kr.saveEntity(newLayer)
             self.__kr.getLayerTree().insert(dbLayer,activeLayer)
             
         except:
-            print "----<<Err>>Unable to create the layer : %s "%layerName
+            print("----<<Err>>Unable to create the layer : %s " % layerName)
     
     def setCurrentLayer(self):
         """
             set the current layer
         """
-        LayerName=raw_input("-->Layer Id :")
+        LayerName = input("-->Layer Id :")
         try:
             self.__kr.getLayerTree().setActiveLayer(LayerName)
         except:
-            print "----<<Err>>Unable to create the layer : %s "%LayerName
+            print("----<<Err>>Unable to create the layer : %s " % LayerName)
             
     def getCurrentLayerName(self):
         """
             get the current layer name
         """
         try:
-            activeLayer=self.__kr.getLayerTree().getActiveLater()
-            ce=self.__kr.getLayerTree()._getLayerConstructionElement(activeLayer)
-            print "Layer Name", ce.name
+            activeLayer = self.__kr.getLayerTree().getActiveLater()
+            ce = self.__kr.getLayerTree()._getLayerConstructionElement(activeLayer)
+            print("Layer Name", ce.name)
         except:
-            print "----<<Err>>unable to get the current layer name "
+            print("----<<Err>>unable to get the current layer name ")
 
     def importExt(self):
         """
